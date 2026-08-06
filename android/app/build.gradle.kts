120f823c314e592b95514f8975c46c35ac63848c
Daffa Jaya Perkasa <251403746+dafayape@users.noreply.github.com>
2026-08-06 14:05:03 +0700

    chore: bump version code to 21 and target SDK to 35 for Google Play release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -39,7 +39,7 @@ android {
     defaultConfig {
         applicationId = "com.sespimma.app"
         minSdk = flutter.minSdkVersion
-        targetSdk = 34
+        targetSdk = 35
         versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true
